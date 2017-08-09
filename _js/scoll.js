8043e23e928d6fde0c62b30e20b9ad399447625a
Kevin Yum <kevinyum95@gmail.com>
2017-08-08 18:34:38 -0700

    add fixed scroll for project div

--- _js/scoll.js
+++ _js/scoll.js
@@ -1,24 +1,46 @@
-var fixmeTop = $('#aboutCol1').offset().top; // get initial position of the element
-var fixmeBottom = $('#aboutCol1').height();
+var aboutTop = $('#aboutCol1').offset().top; // get initial position of the element
+var aboutBottom = $('#aboutCol1').height();
+//var aboutMargin = 
+
+var projectTop = $('#projectCol1').offset().top;
+var projectBottom = $('#projectCol1').height() + projectTop - 750;
 
 $(window).scroll(function () { // assign scroll event listener
 
   var currentScroll = $(window).scrollTop(); // get current position
 
-  if (currentScroll >= fixmeTop && currentScroll <= fixmeBottom) { // apply position: fixed if you
+  if (currentScroll >= aboutTop && currentScroll <= aboutBottom) { // apply position: fixed if you
     $('.callout').css({ // scroll to that element or below it
       position: 'fixed',
       top: '0px',
       left: '300px'
     });
-    $('.bottom').css({
+    $('.aboutSbottom').css({
       visibility: 'hidden'
     });
   } else { // apply position: static
     $('.callout').css({ // if you scroll above it
       position: 'static'
     });
-    $('.bottom').css({
+    $('.aboutSbottom').css({
+      visibility: 'visible'
+    });
+  }
+  
+  if (currentScroll >= projectTop && currentScroll <= projectBottom) { // apply position: fixed if you
+    $('.projectScroll').css({ // scroll to that element or below it
+      position: 'fixed',
+      top: '0px',
+      left: '294px'
+    });
+    $('.projectSbottom').css({
+      visibility: 'hidden'
+    });
+  } else { // apply position: static
+    $('.projectScroll').css({ // if you scroll above it
+      position: 'static'
+    });
+    $('.projectSbottom').css({
       visibility: 'visible'
     });
   }
