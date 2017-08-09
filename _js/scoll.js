1e342959932128a590bcb2fe4fa345b8809ff48b
Kevin Yum <kevinyum95@gmail.com>
2017-08-08 17:29:03 -0700

    add scroll.js
    makes subject stay in center of div while scrolling in that div

--- _js/scoll.js
+++ _js/scoll.js
@@ -1,8 +1,26 @@
-   // Cache selectors outside callback for performance. 
-   var $window = $(window),
-     $stickyEl = $('.about'),
-     elTop = $stickyEl.offset().top;
+var fixmeTop = $('#aboutCol1').offset().top; // get initial position of the element
+var fixmeBottom = $('#aboutCol1').height();
 
-   $window.scroll(function () {
-     $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-   });
+$(window).scroll(function () { // assign scroll event listener
+
+  var currentScroll = $(window).scrollTop(); // get current position
+
+  if (currentScroll >= fixmeTop && currentScroll <= fixmeBottom) { // apply position: fixed if you
+    $('.callout').css({ // scroll to that element or below it
+      position: 'fixed',
+      top: '0px',
+      left: '300px'
+    });
+    $('.bottom').css({
+      visibility: 'hidden'
+    });
+  } else { // apply position: static
+    $('.callout').css({ // if you scroll above it
+      position: 'static'
+    });
+    $('.bottom').css({
+      visibility: 'visible'
+    });
+  }
+
+});
